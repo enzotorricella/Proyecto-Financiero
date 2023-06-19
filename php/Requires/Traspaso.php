3e1a9f5fb847cf8677416d039e59bffe4c37bff9
enzo torricella <enzo.torricella25@gmail.com>
2023-06-19 15:21:23 -0300

    traspaso

--- php/Requires/Traspaso.php
+++ php/Requires/Traspaso.php
@@ -1,2 +1,10 @@
-#traspaso de datos 
-#recepcion de datos por formulario y escritura dn base de datos 
+<?php
+$categoria = $_POST['categoria'];
+$monto = $_POST['monto'];
+$descripcion = $_POST['descripcion'];
+
+// Hacer algo con los valores recibidos
+echo "Categoría: " . $categoria . "<br>";
+echo "Monto: " . $monto . "<br>";
+echo "Descripción: " . $descripcion;
+?>
