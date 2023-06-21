431995ecf3aafb36cd527f10d8612a3c948bcb2d
enzo torricella <enzo.torricella25@gmail.com>
2023-06-21 09:26:48 -0300

    base de datos

--- php/Requires/Traspaso.php
+++ php/Requires/Traspaso.php
@@ -1,10 +1,26 @@
 <?php
+//Conexion a bae de datos en otro script
+require 'Conect.php';
+
 $categoria = $_POST['categoria'];
 $monto = $_POST['monto'];
 $descripcion = $_POST['descripcion'];
 
-// Hacer algo con los valores recibidos
-echo "Categoría: " . $categoria . "<br>";
-echo "Monto: " . $monto . "<br>";
-echo "Descripción: " . $descripcion;
+// Escapar caracteres especiales para evitar inyecciones SQL
+$categoria = $conn->real_escape_string($categoria);
+$monto = $conn->real_escape_string($monto);
+$descripcion = $conn->real_escape_string($descripcion);
+
+// Crear la consulta de inserción
+$sql = "INSERT INTO nombre_tabla (categoria, monto, descripcion) VALUES ('$categoria', '$monto', '$descripcion')";
+
+// Ejecutar la consulta de inserción
+if ($conn->query($sql) === TRUE) {
+    echo "Datos insertados correctamente en la base de datos.";
+} else {
+    echo "Error al insertar los datos: " . $conn->error;
+}
+
+// Cerrar la conexión
+$conn->close();
 ?>
