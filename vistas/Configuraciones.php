546926f1ed298f83fe57f01439a13c4dcd706118
enzo <enzo.torricella25@gmail.com>
2023-07-19 15:20:09 -0300

    gr

--- vistas/Configuraciones.php
+++ vistas/Configuraciones.php
@@ -1,60 +1,64 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Panel de Control Financiero</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
-<?php require_once 'Modif_vista\Nav.php'; ?>
+
+  <?php require_once 'Modif_vista\Nav.php'; ?>
 
   <br>
-<div class="container-fluid">
-    
-  <div class="container">
-    <h1>Configuraciones</h1>
-    <form>
-      <div class="form-group">
-        <label for="moneda">Moneda por defecto:</label>
-        <select class="form-control" id="moneda">
-          <option>USD</option>
-          <option>EUR</option>
-          <option>GBP</option>
-          <option>JPY</option>
-          <option>ARS</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="nombreUsuario">Cambiar nombre de usuario:</label>
-        <input type="text" class="form-control" id="nombreUsuario">
-      </div>
-      <div class="form-group">
-        <label for="contrasena">Modificar contraseña:</label>
-        <input type="password" class="form-control" id="contrasena">
-      </div>
-      <div class="form-group">
-        <label for="presupuesto">Presupuesto mensual:</label>
-        <input type="number" class="form-control" id="presupuesto">
-      </div>
-      <div class="form-group">
-        <label for="presupuesto">Meta de ahorro:</label>
-        <input type="number" class="form-control" id="presupuesto">
-      </div>
-      <div class="form-group">
-        <label for="idioma">Configuración de idioma:</label>
-        <select class="form-control" id="idioma">
-          <option>Español</option>
-          <option>Inglés</option>
-          <option>Alemán</option>
-          <option>Francés</option>
-        </select>
-      </div>
-      <button type="submit" class="btn btn-primary">Guardar configuraciones</button>
-    </form>
+  <div class="container-fluid">
+
+    <div class="container">
+      <h1>Configuraciones</h1>
+      <form>
+        <div class="form-group">
+          <label for="moneda">Moneda por defecto:</label>
+          <select class="form-control" id="moneda">
+            <option>USD</option>
+            <option>EUR</option>
+            <option>GBP</option>
+            <option>JPY</option>
+            <option>ARS</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="nombreUsuario">Cambiar nombre de usuario:</label>
+          <input type="text" class="form-control" id="nombreUsuario">
+        </div>
+        <div class="form-group">
+          <label for="contrasena">Modificar contraseña:</label>
+          <input type="password" class="form-control" id="contrasena">
+        </div>
+        <div class="form-group">
+          <label for="presupuesto">Presupuesto mensual:</label>
+          <input type="number" class="form-control" id="presupuesto">
+        </div>
+        <div class="form-group">
+          <label for="presupuesto">Meta de ahorro:</label>
+          <input type="number" class="form-control" id="presupuesto">
+        </div>
+        <div class="form-group">
+          <label for="idioma">Configuración de idioma:</label>
+          <select class="form-control" id="idioma">
+            <option>Español</option>
+            <option>Inglés</option>
+            <option>Alemán</option>
+            <option>Francés</option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar configuraciones</button>
+      </form>
+    </div>
   </div>
-</div>
 
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- vistas/Informes.php
+++ vistas/Informes.php
@@ -1,41 +1,42 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Panel de Control Financiero</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
   <?php require_once 'Modif_vista\Nav.php'; ?>
-  <br>
-      <div class="container">
-        
-      <div class="card-group">
-        <div class="card">
-          <img src="img/empresaria-usando-tableta-analizar-concepto-exito-estadisticas-estrategia-financiera-empresa-planificacion-futuro-sala-oficina.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Informe de Distribución de Gastos</h5>
-            <p class="card-text">Analiza cómo se distribuyen tus gastos en diferentes categorías.</p>
-            <button type="button" class="btn btn-outline-dark" href="#">Descargar</button>
-          </div>
-        </div>
-        <div class="card">
-            <img src="img/empresaria-usando-tableta-analizar-concepto-exito-estadisticas-estrategia-financiera-empresa-planificacion-futuro-sala-oficina.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Informe Mensual de Transacciones</h5>
-            <p class="card-text">Muestra ingresos, egresos y el porcentaje de ahorros mensuales.</p>
-            <button type="button" class="btn btn-outline-dark" href="#">Descargar</button>
-          </div>
-        </div>
-        <div class="card">
-          <img src="img/empresaria-usando-tableta-analizar-concepto-exito-estadisticas-estrategia-financiera-empresa-planificacion-futuro-sala-oficina.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Informe de Comparación de Gastos Mensuales</h5>
-            <p class="card-text">Compara tus gastos mensuales a lo largo del tiempo.</p>
-            <button type="button" class="btn btn-outline-dark" href="#">Descargar</button>
-          </div>
+  <div class="container">
+
+    <div class="card-group">
+      <div class="card">
+        <img src="img/empresaria-usando-tableta-analizar-concepto-exito-estadisticas-estrategia-financiera-empresa-planificacion-futuro-sala-oficina.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Informe de Distribución de Gastos</h5>
+          <p class="card-text">Analiza cómo se distribuyen tus gastos en diferentes categorías.</p>
+          <button type="button" class="btn btn-outline-dark" href="#">Descargar</button>
         </div>
       </div>
-   
+      <div class="card">
+        <img src="img/empresaria-usando-tableta-analizar-concepto-exito-estadisticas-estrategia-financiera-empresa-planificacion-futuro-sala-oficina.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Informe Mensual de Transacciones</h5>
+          <p class="card-text">Muestra ingresos, egresos y el porcentaje de ahorros mensuales.</p>
+          <button type="button" class="btn btn-outline-dark" href="#">Descargar</button>
+        </div>
       </div>
+      <div class="card">
+        <img src="img/empresaria-usando-tableta-analizar-concepto-exito-estadisticas-estrategia-financiera-empresa-planificacion-futuro-sala-oficina.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Informe de Comparación de Gastos Mensuales</h5>
+          <p class="card-text">Compara tus gastos mensuales a lo largo del tiempo.</p>
+          <button type="button" class="btn btn-outline-dark" href="#">Descargar</button>
+        </div>
+      </div>
+    </div>
+
+  </div>
