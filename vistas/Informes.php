37ba0f108533b120575ab153f645f19c2df199f2
enzo <enzo.torricella25@gmail.com>
2023-07-24 11:01:26 -0300

    45697

--- vistas/Informes.php
+++ vistas/Informes.php
@@ -9,7 +9,28 @@
 </head>
 
 <body>
-  <?php require_once 'Modif_vista\Nav.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand">Panel de Control</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="vistas\Transacciones.php">Transacciones</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="vistas\Informes.php">Informes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="vistas\Configuraciones.php">Configuración</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <div class="container">
 
     <div class="card-group">
